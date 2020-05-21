221000e456167d76b63a57f2d8be39a8d0234157
Mariandrea Maldonado <mariandrea.mv15@gmail.com>
2020-05-21 13:26:37 -0400

    building contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,40 +6,40 @@ class Contact extends Component{
     return(
 
       <div className = "profile cont">
-        <form>
+        <form name="contact" method ="POST" data-netlify="true">
           <div className ="center">
           <h2 className = "p-title">I'll contact you.</h2>
           </div>
           <div  className = "two">
               <div className = "input">
                 <label>Name*</label>
-                <input tupe="text"></input>
+                <input tupe="text" name="name" required></input>
               </div>
               <div className = "input">
                 <label>Last Name*</label>
-                <input type = "text" ></input>
+                <input type = "text" name="last_name" required></input>
               </div>
           </div>
           <div  className = "two">
               <div className = "input">
                 <label>Phone*</label>
-                <input tupe="number"></input>
+                <input tupe="number" name="phone" required></input>
               </div>
               <div className = "input">
                 <label>Company</label>
-                <input type = "text" ></input>
+                <input type = "text" name="company" ></input>
               </div>
           </div>
           <div className = "input">
             <label>E-mail*</label>
-            <input type="e-mail"></input>
+            <input type="email" name="email" required></input>
           </div>
           <div className = "input">
             <label>What is your project about?</label>
-            <textarea rows="4"></textarea>
+            <textarea rows="4" name="about"></textarea>
           </div>
           <div className = "center">
-            <button className = "button p-c">Send</button>
+            <button className = "button p-c" type="submit">Send</button>
           </div>
         </form>
       </div>

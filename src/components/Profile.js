79e60e9f55f97eac79a84fe4c052a246749190cc
Mariandrea Maldonado <mariandrea.mv15@gmail.com>
2021-06-28 14:49:25 -0400

    adding prettier and cleaning

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,17 +1,16 @@
-import React from 'react';
-import Photo from '../images/yo3.jpg';
-
+import React from "react";
+import Photo from "../images/yo3.jpg";
 
 const Profile = () => {
-  return(
-    <div className = "profile">
-      <img className="profile-img" src ={Photo} alt ="profile photo"></img>
-      <div className ="half-p pink-bg">
-        <h2 className = "p-c">I'm a Front-end Developer</h2>
+  return (
+    <div className="profile">
+      <img className="profile-img" src={Photo} alt="profile"></img>
+      <div className="half-p pink-bg">
+        <h2 className="p-c">I'm a Front-end Developer</h2>
         <p>I'm always learning</p>
       </div>
     </div>
   );
-}
+};
 
 export default Profile;
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,45 +1,56 @@
-import React, {Component} from 'react';
-import {Router, NavLink} from 'react-router-dom';
+import React, { Component } from "react";
+import { NavLink } from "react-router-dom";
 
-const Projects = (props) =>{
-
-    let data = props.projects;
-    console.log(data);
-    return(
-      <div>
-      <h3 className = "title p-c">Projects</h3>
-      <div className = " box ">
-      <div className = " table">
-        {
-           data.map( (project) =>
+const Projects = (props) => {
+  let data = props.projects;
+  console.log(data);
+  return (
+    <div>
+      <h3 className="title p-c">Projects</h3>
+      <div className=" box ">
+        <div className=" table">
+          {data.map((project) => (
             <Project
               title={project.fields.title}
               description={project.fields.briefDescription}
-              photos = {project.fields.gallery}
+              photos={project.fields.gallery}
               website={project.fields.website}
-              id = {project.fields.id}
-              key = {project.fields.id}
-            />)
-          }
+              id={project.fields.id}
+              key={project.fields.id}
+            />
+          ))}
+        </div>
       </div>
     </div>
-    </div>
-    );
-}
+  );
+};
 
-class  Project extends Component {
-render(){
-  return(
-    <section className = "col">
-      <img className = "image-project" src ={this.props.photos[0].url}S alt ="project photo"></img>
-      <h2 className = "p-title p-c">{this.props.title}</h2>
-      <small className="p-c">{this.props.description}</small>
-      <div className="two">
-        <a href ={this.props.website} target="_blank"><button className = "button p-c">Website</button></a>
-        <NavLink to = {`/Projects/${this.props.id}`}><button className = "button p-c">Description</button></NavLink>
-      </div>
-    </section>
-  );}
+class Project extends Component {
+  render() {
+    return (
+      <section className="col">
+        <img
+          className="image-project"
+          src={this.props.photos[0].url}
+          alt="project"
+        ></img>
+        <h2 className="p-title p-c">{this.props.title}</h2>
+        <small className="p-c">{this.props.description}</small>
+        <div className="two">
+          <a
+            href={this.props.website}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <button className="button p-c">Website</button>
+          </a>
+          <NavLink to={`/Projects/${this.props.id}`}>
+            <button className="button p-c">Description</button>
+          </NavLink>
+        </div>
+      </section>
+    );
+  }
 }
 
 export default Projects;

ebe64013ca2f4064bd3c61a53e6330113a38c3a2
Mariandrea Maldonado <mariandrea.mv15@gmail.com>
2021-08-24 14:21:54 -0400

    updating title

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -6,7 +6,7 @@ const Profile = () => {
     <div className="profile">
       <img className="profile-img" src={Photo} alt="profile"></img>
       <div className="half-p pink-bg">
-        <h2 className="p-c">I'm a Front-end Developer</h2>
+        <h2 className="p-c">I'm a Software Developer</h2>
         <p>I'm always learning</p>
       </div>
     </div>
